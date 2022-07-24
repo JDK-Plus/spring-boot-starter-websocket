84d578997b2a2e286fec0d839c0a4bccedcb41d9
[moon] <[moon@jdk.plus]>
2022-07-24 14:31:03 +0800

    添加消息推送回调

--- src/main/java/plus/jdk/websocket/global/UserChannelConnectSynchronizer.java
+++ src/main/java/plus/jdk/websocket/global/UserChannelConnectSynchronizer.java
@@ -1,16 +1,12 @@
 package plus.jdk.websocket.global;
 
-import com.google.gson.Gson;
 import com.google.protobuf.ByteString;
-import com.google.protobuf.InvalidProtocolBufferException;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.scheduling.Trigger;
-import org.springframework.scheduling.annotation.SchedulingConfigurer;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
-import org.springframework.scheduling.support.PeriodicTrigger;
 import plus.jdk.broadcast.broadcaster.UdpBroadcastMessageMonitor;
 import plus.jdk.broadcast.broadcaster.UdpMessageBroadcaster;
 import plus.jdk.broadcast.broadcaster.model.BroadcastMessage;
@@ -71,26 +67,41 @@ public class UserChannelConnectSynchronizer implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if(properties.getBroadcastMonitorPort() <= 0) {
+        if (properties.getBroadcastMonitorPort() <= 0) {
             return;
         }
         Thread thread = new Thread(() -> udpBroadcastMessageMonitor.subscribe((ctx, msg) -> {
             WsMessage wsMessage = WsMessage.parseFrom(msg.getContent());
             SessionGroupManager sessionGroupManager = beanFactory.getBean(SessionGroupManager.class);
             ConcurrentLinkedDeque<IWsSession<?>> sessions = sessionGroupManager.getSession(wsMessage.getUid(), wsMessage.getPath());
-            if(properties.getPrintBroadcastMessage()) {
+            if (properties.getPrintBroadcastMessage()) {
                 log.info("receive broadcast message: {}", wsMessage);
             }
             for (IWsSession<?> session : sessions) {
-                if(MessageType.MESSAGE_TYPE_TEXT.equals(wsMessage.getType())) {
-                    session.sendText(new String(wsMessage.getData().toByteArray()));
+                ChannelFuture future = null;
+                if (MessageType.MESSAGE_TYPE_TEXT.equals(wsMessage.getType())) {
+                    future = session.sendText(new String(wsMessage.getData().toByteArray()));
                 }
-                if(MessageType.MESSAGE_TYPE_BINARY.equals(wsMessage.getType())) {
-                    session.sendBinary(wsMessage.getData().toByteArray());
+                if (MessageType.MESSAGE_TYPE_BINARY.equals(wsMessage.getType())) {
+                    future = session.sendBinary(wsMessage.getData().toByteArray());
                 }
+                if (future == null) {
+                    continue;
+                }
+                future.addListener((ChannelFutureListener) channelFuture -> {
+                    if(properties.getMessagePushPromise() == null) {
+                        return;
+                    }
+                    try {
+                        IBroadMessagePromise promise = beanFactory.getBean(properties.getMessagePushPromise());
+                        promise.onCompletion(channelFuture.isSuccess(), wsMessage,  session);
+                    }catch(Exception e) {
+                        log.error(e.getMessage());
+                    }
+                });
             }
             return true;
         }));
-        thread.start();
+        Runtime.getRuntime().addShutdownHook(thread);
     }
 }
